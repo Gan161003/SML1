98feb107b2f93906cf6ceb60ab9b2e35bd77fd8b
Gan161003 <149460605+Gan161003@users.noreply.github.com>
2026-06-18 09:44:50 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,17 +52,27 @@ app = FastAPI(
 )
 
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:5173",
+#         "http://127.0.0.1:5173",
+#         "https://sml-backend-agdr.onrender.com/"
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://sml-backend-agdr.onrender.com/"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 app.include_router(
     connector_router,
     prefix="/connectors",
